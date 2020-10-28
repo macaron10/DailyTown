4364d888a8ed05592cc6e222d0b82dfc987db9e3
임승현 <limseung10@gmail.com>
2020-10-28 18:00:07 +0900

    Feat (F) | Inventory 컴포넌트 생성

---
+++ frontend/components/Inventory.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet, View } from 'react-native';
+
+export default function Inventory() {
+  return (
+    <View style={ styles.testGrid }>
+      <View style={ styles.testGridContainer }>
+        {[0, 1, 2, 3].map((number) =>
+          <View key={number.toString()} style={ styles.testGridRow }>
+            {[0, 1, 2 ,3 ,4].map((number) =>
+              <View key={number.toString()} style={ styles.testGridCell }>
+              </View>
+            )}
+          </View>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  testGrid: {
+    color: '#776e65',
+    position: 'relative',
+    backgroundColor: '#bbada0',
+    width: '100%',
+    height: 300,
+  },
+  testGridContainer: {
+    color: '#776e65',
+    position: 'absolute',
+    zIndex: 1,
+  },
+  testGridRow: {
+    color: '#776e65',
+    paddingHorizontal: 10,
+    paddingVertical: 9,
+    flexDirection: 'row',
+  },
+  testGridCell: {
+    color: '#776e65',
+    width: 56.25,
+    height: 56.25,
+    marginRight: 15,
+    borderRadius: 3,
+    backgroundColor: 'rgba(238, 228, 218, 0.35)',
+  }
+
+})
+
