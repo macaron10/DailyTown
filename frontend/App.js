2f06fd13523fa2981f45990788356790e1b0df9f
Hona <choihs2393@gmail.com>
2020-10-28 17:27:55 +0900

    Feat(FE) | Create testmodal

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MissionModal from './components/MissionModal';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>공허의 숲!!!</Text>
       <StatusBar style="auto" />
+      <MissionModal />
     </View>
   );
 }
