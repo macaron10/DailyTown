fde6968a80c3b61c78f8a89e4e30e5eb71c379fc
임승현 <limseung10@gmail.com>
2020-10-28 18:00:39 +0900

    Feat (F) | Inventory 버튼 생성

--- frontend/App.js
+++ frontend/App.js
@@ -6,6 +6,22 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.containerUnder}>
+        <View style={{ flexDirection: 'row' }}>
+          <TouchableOpacity
+            onPress={() => alert('Hello, world!')}
+            style={{ backgroundColor: 'red', width: '50%' }}
+          >
+            <Text style={{ color: '#fff', textAlign: 'center' }}>Inven</Text>
+
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => alert('Hello, world!')}
+            style={{ backgroundColor: 'blue', width: '50%',  }}
+          >
+            <Text style={{ color: '#fff', textAlign: 'center' }}>Store</Text>
+
+          </TouchableOpacity>        
+        </View>
         <Inventory/>
       </View>
 
