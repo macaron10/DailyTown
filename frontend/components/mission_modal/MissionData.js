9e26be15ebf761980c26abc22364dbb950125f4f
Hona <choihs2393@gmail.com>
2020-11-02 16:14:33 +0900

    Style(FE) | item 이미지 추가

--- frontend/components/mission_modal/MissionData.js
+++ frontend/components/mission_modal/MissionData.js
@@ -2,19 +2,22 @@ export default [
   {
     bg: "#086E4B",
     color: "#FCBE4A",
-    title: "Mission 1",
+    title: "꽃 사진을 찍으세요.",
     contents: "mission 1 contents",
+    // imgUrl: "../../assets/test_img/노랑뚱땡이.png"
   },
   {
     bg: "#A8DDE9",
     color: "#3F5B98",
-    title: "Mission 2",
-    contents: "mission 2 contents"
+    title: "나무 사진을 찍으세요.",
+    contents: "mission 2 contents",
+    // imgUrl: "../../assets/test_img/노랑뚱땡이.png"
   },
   {
     bg: "#A8DDE9",
     color: "#3F5B98",
-    title: "Mission 3",
+    title: "사람 사진을 찍으세요.",
     contents: "mission 3 contents",
+    // imgUrl: "../../assets/test_img/노랑뚱땡이.png"
   },
 ]
