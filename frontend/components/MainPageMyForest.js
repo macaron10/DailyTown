17afccbb8863ca00befb39efb753a75f85f426e1
Eom hongjae <ox1351@naver.com>
2020-11-03 21:37:04 +0900

    Fix(FE) resolveAssetSource web호환 문제(web인 경우 임시값 넣어놓음)

--- frontend/components/MainPageMyForest.js
+++ frontend/components/MainPageMyForest.js
@@ -1,26 +1,34 @@
 import React from 'react'
-import {Image, Dimensions, TouchableWithoutFeedback} from 'react-native'
+import {Image, Dimensions, TouchableWithoutFeedback, Alert, Platform} from 'react-native'
 
 // Data를 Array로 입력받는다고 가정
 const data = [
-  { x: 0, y: 0 },
+  { x: 1, y: 0 },
   { x: 2, y: 1 },
   { x: 8, y: 8 },
   { x: 7, y: 4 },
   { x: 5, y: 4 },
 ]
 
-
 export default function MyForest(props) {
   const deviceWidth = Dimensions.get('window').width
   const width = (deviceWidth - 20) / 9  
-  const onPressIn = () => { console.log('in')}
+  const onPressIn = () => {alert('hi')}
   const onPressOut = () => { console.log('out') }
   return (
     data.map((d) => {
       const url = require('../assets/test_img/tree.png')
-      const image = Image.resolveAssetSource(url)
-      const height = image.height*width/image.width
+      let height
+      if (Platform.OS==='web') {
+        height = 40
+      } else {
+        const image = Image.resolveAssetSource(url)
+        height = image.height*width/image.width
+      }
+      // 개발환경을 위한 분기 web인 경우 임시의 숫자 넣어놓음
+      // 웹에서는 resolveAssetSource 오류
+      // const image = Image.resolveAssetSource(url)
+      // const height = image.height*width/image.width
       return (
         <TouchableWithoutFeedback key={`${d.x}${d.y}`}
           onPressIn={() => { onPressIn() }}
