dbb817f2fe80395864c07dd357726f631a2f453c
Kanggyu.Min <macaron61@gmail.com>
2020-11-05 15:45:49 +0900

    Refactor(FE) | Logout 함수 분리

--- frontend/screens/Main.js
+++ frontend/screens/Main.js
@@ -9,47 +9,37 @@ import MainPageInventory from '../components/mainbottom/MainPageInventory'
 import MissionModal from '../components/mission_modal/MissionModal';
 import MyGold from '../components/MyGold';
 
+import { logout } from '../utils/authentication';
+
 export default function Main({navigation}) {
   const [goldStatus, setGoldStatus] = useState(10000)
 
 
   return (
     <View style={ styles.container }>
+      {/* 로그인 페이지로 이동 버튼(임시) */}
       <TouchableOpacity
         style={ styles.devbutton }
         onPress={() => navigation.navigate('Login')}
       >
         <Text style={{ color: '#fff', textAlign: 'center' }}>LoginPage(for Dev)</Text>
       </TouchableOpacity>
+      {/* 토큰 저장소 확인버튼(임시) */}
       <TouchableOpacity
         style={{ position: 'absolute', top:100, backgroundColor: '#000000', zIndex:100}}
         onPress={async () => {
-          const test = await SecureStore.getItemAsync('token')
-          console.log(test,'이게 스토어의 토큰');
-          const test2 = await SecureStore.getItemAsync('access_token')
-          console.log(test2,'이게 스토어의 액세스 토큰');
+          const jwt = await SecureStore.getItemAsync('token')
+          console.log(jwt,'이게 스토어의 토큰');
+          const acst = await SecureStore.getItemAsync('access_token')
+          console.log(acst,'이게 스토어의 액세스 토큰');
         }}
       >
         <Text style={{ color: '#fff', textAlign: 'center' }}>저장값꺼내기</Text>
       </TouchableOpacity>
+      {/* 로그아웃 버튼 */}
       <TouchableOpacity
         style={ styles.logoutButton }
-        onPress={async () => {
-          const accessToken = await SecureStore.getItemAsync('access_token')
-          await Google.logOutAsync({ accessToken, androidClientId: env.AND_KEY}); // 나중에 따로 config 설정해줘야함
-          // ------------------------ access token 만료 확인용 -------------------------------
-          // let userInfoResponse = await fetch('https://www.googleapis.com/userinfo/v2/me', {
-          //   headers: { Authorization: `Bearer ${accessToken}` },
-          // })
-          // .then(
-          //   res => res.json()
-          // )
-          // .then(
-          //   json => {console.log(json)}
-          // );
-          await SecureStore.deleteItemAsync('token')
-          await SecureStore.deleteItemAsync('access_token')
-        }}
+        onPress={logout()}
       >
         <Text style={{ color: '#fff', textAlign: 'center' }}>Logout</Text>
       </TouchableOpacity>
