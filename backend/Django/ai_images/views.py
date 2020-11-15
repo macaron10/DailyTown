6ffe72ea25f1b59b5dabff4e133a9751a8a60c44
이태환 <lwh1992@naver.com>
2020-11-15 16:05:58 +0900

    Feat(BE)|Cron upload

--- backend/Django/ai_images/views.py
+++ backend/Django/ai_images/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from django.http import JsonResponse
 
+import MM
+
 from .Keras_model.fruit_model.classification import check_fruit
 from .Keras_model.general_model.classification_model import check_general
 
@@ -11,19 +13,21 @@ def predict(request):
 	}
 	# category == 물건의 종류, title == 물건의 detail
 
-	print(request)
-	print(request.data)
-	print(request.FILES)
-	print(request.FILES['image'])
+	# print(request)
+	# print(request.data)
+	# print(request.FILES)
+	# print(request.FILES['image'])
+	try:
+		keyword = request.data['category']
+		target = request.data['title']
+	except Exception as e:
+		MM.send(e, "ai/")
 
-	keyword = request.data['category']
-	target = request.data['title']
-
-	# 물건 종류에 따라 if문 model 분기
+		# 물건 종류에 따라 if문 model 분기
 	if keyword == 'fruit':
 		result['ans'] = check_fruit(request, target)
 	elif keyword == 'general':
-  		result['ans'] = check_general(request, target)
+		result['ans'] = check_general(request, target)
 	# test용 print문.
 	
 	# qwnkld(request.FILES['image'])
