dc09683443fc3bf9528c6517bfa5a332267c18e6
이태환 <lwh1992@naver.com>
2020-11-18 00:34:40 +0900

    Feat(BE)|docker-compose 정리

--- backend/Django/bexperts/settings.py
+++ backend/Django/bexperts/settings.py
@@ -20,11 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os, json
 from django.core.exceptions import ImproperlyConfigured
 
+# SECRET_KEY 파일 위치
 secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(secret_file) as f:
-    secret_file = json.loads(f.read())
+    secrets = json.loads(f.read())
 
+# secrets.json 파일에서 SECRET_KEY 가져오기    
 def get_secret(setting, secrets=secrets):
     try:
         return secrets[setting]
