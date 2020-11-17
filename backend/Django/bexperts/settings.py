4c4dbb41e723565539f85c5d332c9feafafd77ee
이태환 <lwh1992@naver.com>
2020-11-18 00:21:54 +0900

    Feat(BE)| Django Secret key 분리

--- backend/Django/bexperts/settings.py
+++ backend/Django/bexperts/settings.py
@@ -17,10 +17,25 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = '&%i6*h!^efcd8yegbrr62hrtzzn)5&@a*rwo^#qb#rs(b*#hiz'
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secret_file = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
